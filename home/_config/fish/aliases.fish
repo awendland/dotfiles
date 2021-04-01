29d2b26b956d36b43b0b6820d50fb01c82ea5b61
Alex Wendland <me@alexwendland.com>
2021-04-01 08:18:10 -0700

    fish: remove unused aliases and functions

--- home/_config/fish/aliases.fish
+++ home/_config/fish/aliases.fish
@@ -30,8 +30,6 @@ switch (uname)
             printf "\n\\033[1;31mALERT:\\033[0m You should \\033[34m'sude apt-get install trash-cli'\\033[0m for better rm practice"
         end
 end
-#A Recommend against rm
-alias rm 'printf "\\033[1;31mThis is not the command you are looking for.\\033[0m\nBut if need be --> \\033[1;34m\\\\rm\\033[0m"; false'
 
 #H Copying/Transfer
 #A Alias for useful rsync command
@@ -63,15 +61,7 @@ alias tf 'terraform'
 #A Alias for chopping lines in less instead of wrapping them
 alias lessnw 'less -S'
 
-#H Cat aliases
-#A Alias for pretty printing a file
-alias pretty 'pygmentize -g'
-
 #H Internet aliases
-#A Alias for Python's SimpleHTTPServer
-alias webserver 'python -m SimpleHTTPServer'
-#A Alias for wget to use server supplied file download name
-alias download 'wget --content-disposition'
 #A Quickly retrieve machine's remote IP from OpenDNS server
 alias wanip 'dig +short myip.opendns.com @resolver1.opendns.com'
 #A Retrieve more robust info about the current wan IP
@@ -98,10 +88,4 @@ switch (uname)
 	    alias showdotfiles 'defaults write com.apple.finder AppleShowAllFiles -bool true; and killall Finder'
 	#A Hide hidden files in Finder
 	    alias hidedotfiles 'defaults write com.apple.finder AppleShowAllFiles -bool false; and killall Finder'
-	#A Open Markdown editor
-	    if test -e /Applications/MacDown.app
-	        alias markdown 'open -a "MacDown"'
-	    else
-	        alias markdown 'There is no Markdown handler setup.'
-	    end
 end
