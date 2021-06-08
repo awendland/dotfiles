4b1a5151796a4a0983838a3e18ad98cfde702452
Alex Wendland <wendland@getpinwheel.com>
2021-06-07 20:38:05 -0400

    scripts: store at unhidden at ~/scripts, not ~/.scripts

--- home/_config/fish/config.fish
+++ home/_config/fish/config.fish
@@ -26,8 +26,8 @@ set -u fish_key_bindings fish_vi_key_bindings
 # set vim as editor
 set -gx EDITOR 'vim'
 
-# add ~/.scripts to path
-set PATH $HOME/.scripts $PATH
+# add ~/scripts to path
+set PATH $HOME/scripts $PATH
 
 # cause less to stay on screen, and print ansi colors
 set -gx LESS '-RX'
